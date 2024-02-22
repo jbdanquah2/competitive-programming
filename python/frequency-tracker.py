504f98ec7515e61aa2a681c4b8d77f54851ac001
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-02-22 00:32:56 +0000

    frequency tracker

--- python/frequency-tracker.py
+++ python/frequency-tracker.py
@@ -1,28 +1,29 @@
 class FrequencyTracker:
 
     def __init__(self):
-        self.tracker = {}
-        
+        self.freq_dict = defaultdict(int) 
+        self.freq = defaultdict(int)
 
     def add(self, number: int) -> None:
-        if number not in self.tracker:
-            self.tracker[number] = 1
-        else:
-            self.tracker[number] += 1
+        if self.freq[self.freq_dict[number]] != 0:
+            self.freq[self.freq_dict[number]] -= 1
+        self.freq_dict[number] += 1
+        self.freq[self.freq_dict[number]] += 1
 
     def deleteOne(self, number: int) -> None:
-
-        freq = self.tracker.get(number, 0)
-        if not freq:
-            return
-        if freq == 1:
-            del self.tracker[number]
-        elif freq > 1:
-            self.tracker[number] -= 1
+        if self.freq_dict[number] > 0:
+            if self.freq[self.freq_dict[number]] != 0:
+                self.freq[self.freq_dict[number]] -= 1
+            self.freq_dict[number] -= 1
+            if self.freq_dict[number] > 0:
+                self.freq[self.freq_dict[number]] += 1
 
     def hasFrequency(self, frequency: int) -> bool:
-        for value in self.tracker.values():
-            if value == frequency:
-                return True
-            
-        return False
+        return self.freq[frequency] > 0
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
+    
