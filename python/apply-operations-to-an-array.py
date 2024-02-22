6c7a9b9670cff92c61bee8b5b5ec5b1ae837817a
John Danquah-Boateng <yjbdanquah@gmail.com>
2024-02-22 15:11:54 +0000

    apply operations

---
+++ python/apply-operations-to-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+
+        for i in range(len(nums)-1):
+
+            if nums[i] == nums[i + 1]:
+                nums[i] = nums[i] * 2
+                nums[i + 1] = 0
+
+        for j in range(len(nums)):
+            if nums[j] == 0:
+                nums.remove(nums[j])
+                nums.append(0)
+
+        return nums
