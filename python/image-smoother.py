7450ffc76753813414253cda3a3ba808286b4ea8
John Danquah-Boateng <yjbdanquah@gmail.com>
2024-02-26 22:05:14 +0000

    image smoother

---
+++ python/image-smoother.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        m, n = len(img), len(img[0])
+        result = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                total, count = 0, 0
+                for dx in [-1, 0, 1]:
+                    for dy in [-1, 0, 1]:
+                        ni, nj = i + dx, j + dy
+                        if 0 <= ni < m and 0 <= nj < n:
+                            total += img[ni][nj]
+                            count += 1
+                result[i][j] = total // count
+
+        return result
