603878ce5eda1b8b795215e777ab1d5d6c82e230
John Danquah-Boateng <yjbdanquah@gmail.com>
2024-02-27 18:03:29 +0000

    sort the people with bubble sort

---
+++ python/sort-the-people.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+
+        n = len(names)
+
+        for i in range(n):
+
+            for j in range(n - i - 1):
+
+                if heights[j] < heights[j + 1]:
+
+                    names[j + 1], names[j] = names[j], names[j + 1]
+                    heights[j + 1], heights[j] = heights[j], heights[j + 1]
+
+        return names
