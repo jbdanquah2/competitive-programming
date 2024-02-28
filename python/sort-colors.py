2a2dbccb84cdc0dedaa0fce93ec3e5654535a376
John Danquah-Boateng <yjbdanquah@gmail.com>
2024-02-28 22:17:19 +0000

    sort colors

---
+++ python/sort-colors.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        n = len(nums)
+        check = True
+
+        for i in range(n):
+            check = False
+            for j in range(n - 1 - i):
+
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+                    check = True
+
+            if not check:
+                break
+=
