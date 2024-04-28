9c24a3e0a75ae16b112c2e25bd97772a74b161ba
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-04-28 22:16:35 +0000

    Created 'A - FIA Heist' by A2SV Hub

---
+++ A2SV Remote G5 Contest 12 28-Apr-2024/A - FIA Heist 135776.py
@@ -0,0 +1,21 @@
+# Problem: A - FIA Heist - https://codeforces.com/gym/520098/problem/A
+
+def decodeString():
+    # Lewa<-isd<-
+    s = input()
+    stack = []
+    i = 1
+    while i < len(s)+1:
+        c = s[i-1]
+    
+        if c == '<' and s[i] == '-':
+            if stack:
+                stack.pop()
+            i += 1
+        else:
+            stack.append(c)
+        i += 1
+
+    return ''.join(stack)
+
+print(decodeString())
