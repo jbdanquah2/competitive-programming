3ce3bf5fd52dc31a8b5236f7dd3078fcfd007715
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-15 02:58:40 +0000

    Created 'A - Binary Imbalance' by A2SV Hub

---
+++ A2SV Remote Contest #23 15-Jul-2024/A - Binary Imbalance 180448.py
@@ -0,0 +1,39 @@
+# Problem: A - Binary Imbalance - https://codeforces.com/gym/535309/problem/A
+
+def main(t, test_cases):
+    results = []
+    
+    for case in test_cases:
+        n, s = case
+        count_0 = s.count('0')
+        count_1 = s.count('1')
+        
+        if count_0 > count_1:
+            results.append("YES")
+        else:
+            can_insert_zero = False
+            for i in range(n - 1):
+                if s[i] != s[i + 1]:
+                    can_insert_zero = True
+                    break
+            
+            if can_insert_zero:
+                results.append("YES")
+            else:
+                results.append("NO")
+    
+    return results
+
+t = int(input())
+test_cases = []
+
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+    test_cases.append((n, s))
+
+results = main(t, test_cases)
+
+for result in results:
+    print(result)
+
