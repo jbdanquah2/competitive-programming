35382ac717c3e70db9f283e16c3fb81b31571acb
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-15 02:58:52 +0000

    Created 'B - Guess the Maximum' by A2SV Hub

---
+++ A2SV Remote Contest #23 15-Jul-2024/B - Guess the Maximum 180449.py
@@ -0,0 +1,22 @@
+# Problem: B - Guess the Maximum - https://codeforces.com/gym/535309/problem/B
+
+def find_maximum_k(t, n, arr):
+   
+    max_k = float('inf')
+    
+    for i in range(1, n):
+        max_k = min(max_k, max(arr[i-1], arr[i]))
+    
+    return max_k-1
+
+t = int(input())
+results = []
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+ 
+    results.append(find_maximum_k(t, n, arr))
+
+for result in results:
+    print(result)
