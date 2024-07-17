f5b3718edb9b8c04dcd74299a96dbc22ddcfe25c
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-17 20:17:03 +0000

    Created 'A - Flag' by A2SV Hub

---
+++ A2SV Remote Contest #20 17-Jul-2024/A - Flag 181385.py
@@ -0,0 +1,20 @@
+# Problem: A - Flag - https://codeforces.com/gym/531416/problem/A
+
+def check_flag(n, m, flag):
+    # Check each row to see if all columns are the same color
+    for row in flag:
+        if len(set(row)) != 1:
+            return "NO"
+    
+    # Check adjacent rows to ensure they have different colors
+    for i in range(n - 1):
+        if flag[i][0] == flag[i + 1][0]:
+            return "NO"
+    
+    return "YES"
+
+# Read input
+n, m = map(int, input().split())
+flag = [input().strip() for _ in range(n)]
+
+print(check_flag(n, m, flag))
