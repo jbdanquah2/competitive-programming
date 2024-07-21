ea27c5c3fc33a869973d3107137218fd0dc04cf7
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-21 13:09:43 +0000

    Created 'A - Hit the Lottery' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 21-Jul-2024/A - Hit the Lottery 182525.py
@@ -0,0 +1,30 @@
+# Problem: A - Hit the Lottery - https://codeforces.com/gym/536373/problem/A
+
+
+
+def main(amount: int): 
+    moves = 0
+    
+    while amount > 0:
+          
+        if amount >= 100: 
+            amount -= 100
+            moves += 1    
+
+        elif amount >= 20: 
+            amount -= 20
+            moves += 1
+        elif amount >= 10: 
+            amount -= 10
+            moves += 1
+        elif amount >= 5: 
+            amount -= 5
+            moves += 1
+        else: 
+            amount -= 1
+            moves += 1
+            
+    return moves
+    
+amount = int(input())
+print(main(amount))
