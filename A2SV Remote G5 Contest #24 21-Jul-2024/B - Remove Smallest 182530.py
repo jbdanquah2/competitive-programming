6c6637defbfc5862b43ff4f3c0a2e64fc46234ed
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-21 13:34:50 +0000

    Created 'B - Remove Smallest' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 21-Jul-2024/B - Remove Smallest 182530.py
@@ -0,0 +1,15 @@
+# Problem: B - Remove Smallest - https://codeforces.com/gym/536373/problem/B
+
+
+def main(arr: list)-> str: 
+    arr.sort()
+    for i in range(1, len(arr)): 
+        if arr[i] - arr[i-1] > 1: 
+            return 'NO'
+    return 'YES'
+
+t = int(input())
+for _ in range(t): 
+    n = int(input())
+    arr = list(map(int, input().split()))
+    print(main(arr))
