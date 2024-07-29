6d7d68e7c1b2143cb1d1e5eb57b8bac903d6cf83
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-29 20:58:48 +0000

    Created 'A - Valid Parentheses' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/A - Valid Parentheses 185873.py
@@ -0,0 +1,19 @@
+# Problem: A - Valid Parentheses - https://codeforces.com/gym/537575/problem/A
+
+def main(s: str) -> int:
+    
+    result = 0
+    open_brackets = 0
+    
+    for i in s:
+        if i == '(':
+            open_brackets += 1
+        else:
+            if open_brackets > 0:
+                open_brackets -= 1
+                result += 2
+                
+    return result
+
+s = input()
+print(main(s))
