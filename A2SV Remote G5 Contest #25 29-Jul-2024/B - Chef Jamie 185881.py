8701cc687cd925dedd8aac748629aac32054855f
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-29 21:18:42 +0000

    Created 'B - Chef Jamie' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/B - Chef Jamie 185881.py
@@ -0,0 +1,19 @@
+# Problem: B - Chef Jamie - https://codeforces.com/gym/537575/problem/B
+
+def solve():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    ordered = sorted(arr)
+    swaps = 0
+    
+    for i in range(n):
+        if arr[i] != ordered[i]:
+            swaps += 1
+
+    print(max(0, swaps - 1))
+
+
+if __name__ == "__main__":
+    solve()
+    
