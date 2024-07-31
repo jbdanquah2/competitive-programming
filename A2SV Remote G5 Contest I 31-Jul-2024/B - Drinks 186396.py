76368f7130d7337f57bce1b337066a6b6423e799
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-31 20:20:12 +0000

    Created 'B - Drinks' by A2SV Hub

---
+++ A2SV Remote G5 Contest I 31-Jul-2024/B - Drinks 186396.py
@@ -0,0 +1,11 @@
+# Problem: B - Drinks - https://codeforces.com/gym/500425/problem/B
+
+n = int(input())
+
+vol_orange_juice_str = input()
+
+vol_orange_juice = [int(num) for num in vol_orange_juice_str.split()]
+
+sum_vol = sum(vol_orange_juice)
+
+print(sum_vol/n)
