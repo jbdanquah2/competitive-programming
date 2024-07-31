19edd8dc5af8e4ca276c5d3ac5bacce85335366c
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-31 20:20:06 +0000

    Created 'C - Radio Station' by A2SV Hub

---
+++ A2SV Remote G5 Contest I 31-Jul-2024/C - Radio Station 186395.py
@@ -0,0 +1,17 @@
+# Problem: C - Radio Station - https://codeforces.com/gym/500425/problem/C
+
+nm = input()
+
+n, m = [int(num) for num in nm.split()]
+
+servers = {}
+
+for i in range(n):
+    name, ip = input().split()
+    servers[ip+';'] = name
+
+for i in range(m):
+    cmd = input()
+    ip = cmd.split()[1]
+    
+    print(f"{cmd} #{servers[ip]}")
