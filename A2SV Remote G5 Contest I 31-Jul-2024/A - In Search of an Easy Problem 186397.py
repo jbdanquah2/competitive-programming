9ed4fc1f274a287bb61ec1b833ba5ec78709279c
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-31 20:20:19 +0000

    Created 'A - In Search of an Easy Problem' by A2SV Hub

---
+++ A2SV Remote G5 Contest I 31-Jul-2024/A - In Search of an Easy Problem 186397.py
@@ -0,0 +1,12 @@
+# Problem: A - In Search of an Easy Problem - https://codeforces.com/gym/500425/problem/A
+
+n = int(input())
+
+responses_str = input()
+
+responses = [int(num) for num in responses_str.split()]
+
+if any(responses):
+    print("HARD")
+else:
+    print("EASY")
