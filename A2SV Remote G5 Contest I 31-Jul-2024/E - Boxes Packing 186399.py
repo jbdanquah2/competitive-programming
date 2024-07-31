de8af3f728b3e7215a4f6cd172e4aaf36ec60c7c
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-07-31 20:22:13 +0000

    Created 'E - Boxes Packing' by A2SV Hub

---
+++ A2SV Remote G5 Contest I 31-Jul-2024/E - Boxes Packing 186399.py
@@ -0,0 +1,15 @@
+# Problem: E - Boxes Packing - https://codeforces.com/gym/500425/problem/E
+
+def min_visible_boxes(n, box_sizes):
+    from collections import Counter
+    
+    size_counts = Counter(box_sizes)
+    
+    max_frequency = max(size_counts.values())
+    
+    return max_frequency
+
+n = int(input())
+box_sizes = list(map(int, input().split()))
+
+print(min_visible_boxes(n, box_sizes))
