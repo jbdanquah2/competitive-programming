6678b5c0dca5b38559de250a939e71b1a34862fb
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-08-05 07:06:02 +0000

    Created 'B - Chat room' by A2SV Hub

---
+++ A2SV Remote Contest #26 05-Aug-2024/B - Chat room 187556.py
@@ -0,0 +1,16 @@
+# Problem: B - Chat room - https://codeforces.com/gym/540348/problem/B
+
+def check_hello(s):
+    target = "hello"
+    target_index = 0
+    
+    for char in s:
+        if char == target[target_index]:
+            target_index += 1
+            if target_index == len(target):
+                return "YES"
+    return "NO"
+
+# Reading input
+s = input().strip()
+print(check_hello(s))
