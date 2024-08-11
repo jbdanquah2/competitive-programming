b053ca65b3a717747afb4634e3d87d51c60cedb9
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-08-11 12:38:10 +0000

    Created 'A - My First Sorting Problem' by A2SV Hub

---
+++ A2SV Remote G5 Contest 27 11-Aug-2024/A - My First Sorting Problem 190659.py
@@ -0,0 +1,24 @@
+# Problem: A - My First Sorting Problem - https://codeforces.com/gym/541399/problem/A
+
+
+def main(arr: list) -> list:
+    
+    if not arr:
+        return []
+    
+    a = arr[0]
+    b = arr[1]
+    
+    if a < b:
+        return [a, b]
+    else:
+        return [b, a]
+   
+t = int(input())
+
+for _ in range(t):
+    a, b = map(int, input().split())
+    
+    result = main([a, b])
+    print(*result)
+    
