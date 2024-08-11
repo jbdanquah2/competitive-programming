d8c88e483fa84cb6530b52d7f3939fe286027dc9
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-08-11 13:17:15 +0000

    Created 'D - Firaol And His Weapon' by A2SV Hub

---
+++ A2SV Remote G5 Contest 27 11-Aug-2024/D - Firaol And His Weapon 190663.py
@@ -0,0 +1,21 @@
+# Problem: D - Firaol And His Weapon - https://codeforces.com/gym/541399/problem/D
+
+def max_resources(n, masses):
+    max_mass = max(masses)
+    
+    frequency = [0] * (max_mass + 1)
+    for mass in masses:
+        frequency[mass] += 1
+    
+    dp = [0] * (max_mass + 1)
+    
+    dp[1] = frequency[1] * 1
+    
+    for i in range(2, max_mass + 1):
+        dp[i] = max(dp[i-1], dp[i-2] + i * frequency[i])
+    
+    return dp[max_mass]
+
+n = int(input())
+masses = list(map(int, input().split()))
+print(max_resources(n, masses))
