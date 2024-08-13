1e39a26f8c8e0c65292b8eb51671455ddfb6eb8d
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-08-13 18:40:02 +0000

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 13-Aug-2024/Fibonacci Number 191267.py
@@ -0,0 +1,16 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        # Base cases
+        if n <= 1:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        
+        return dp[n]
