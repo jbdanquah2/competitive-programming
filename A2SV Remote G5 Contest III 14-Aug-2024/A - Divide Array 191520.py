fb0dcbee00debbaa3712534b85ad3553ed58d9fe
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-08-14 16:44:34 +0000

    Created 'A - Divide Array' by A2SV Hub

---
+++ A2SV Remote G5 Contest III 14-Aug-2024/A - Divide Array 191520.py
@@ -0,0 +1,35 @@
+# Problem: A - Divide Array - https://codeforces.com/gym/505936/problem/A
+
+
+def main(arr: list, n: int):
+    
+    negative = []
+    positive = []
+    zero = []
+    
+    for i in range(n):
+        if arr[i] < 0:
+            negative.append(arr[i])
+        elif arr[i] > 0:
+            positive.append(arr[i])
+        else:
+            zero.append(arr[i])
+            
+    if len(positive) == 0:
+        positive.append(negative.pop())
+        positive.append(negative.pop())
+        
+    if len(negative) % 2 == 0:
+        zero.append(negative.pop())
+
+    print(len(negative), end=" ")
+    print(*negative)
+    print(len(positive), end=" ")
+    print(*positive)
+    print(len(zero), end=" ")
+    print(*zero)
+    
+  
+n = int(input())  
+arr = list(map(int, input().split()))
+main(arr, n)
