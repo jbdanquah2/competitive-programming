6433d993cbcdc17d29a373e0db17821da1ac21d0
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-08-15 20:00:42 +0000

    Created 'Distinct Subsequences' by A2SV Hub

---
+++ 15-Aug-2024/Distinct Subsequences 191943.py
@@ -0,0 +1,22 @@
+# Problem: Distinct Subsequences - https://leetcode.com/problems/distinct-subsequences/description/
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+    
+        # Initialize the dp table with 0's
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        # Base case: An empty string t can always be formed from any substring of s
+        for i in range(m + 1):
+            dp[i][0] = 1
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[m][n]
+            
