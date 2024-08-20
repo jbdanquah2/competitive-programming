2dfb3f328ddb161c6bed32f327c6837ed8de1dd4
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-08-20 09:21:42 +0000

    Created 'A - Maximum Increase' by A2SV Hub

---
+++ A2SV Remote G5 Education Contest #28 20-Aug-2024/A - Maximum Increase 198101.py
@@ -0,0 +1,20 @@
+# Problem: A - Maximum Increase - https://codeforces.com/gym/543262/problem/A
+
+
+def main(n, arr: list) -> int:
+    if n == 0:
+        return 0
+
+  
+    dp = [1] * (n+1)
+
+    for i in range(1, n):
+        if arr[i] > arr[i-1]:
+            dp[i] = dp[i-1] + 1
+
+    return max(dp)
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+print(main(n, arr))
