feeff0d9a64808f3d8027cf4b22ff56b38c5ca4f
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-08-21 22:51:22 +0000

    Created 'Unique Paths II' by A2SV Hub

---
+++ 21-Aug-2024/Unique Paths II 198569.py
@@ -0,0 +1,22 @@
+# Problem: Unique Paths II - https://leetcode.com/problems/unique-paths-ii/
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        
+        dp = [[0] * n for _ in range(m)]
+        
+        dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+        
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0  # No path through an obstacle
+                else:
+                    if i > 0:
+                        dp[i][j] += dp[i-1][j]  # Paths from above
+                    if j > 0:
+                        dp[i][j] += dp[i][j-1]  # Paths from the left
+        
+        return dp[m-1][n-1]
+            
