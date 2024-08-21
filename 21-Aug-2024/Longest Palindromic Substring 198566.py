45ac3fb97c6db485d6f19c908a07f623001290e3
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-08-21 22:14:50 +0000

    Created 'Longest Palindromic Substring' by A2SV Hub

---
+++ 21-Aug-2024/Longest Palindromic Substring 198566.py
@@ -0,0 +1,26 @@
+# Problem: Longest Palindromic Substring - https://leetcode.com/problems/longest-palindromic-substring/
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 0:
+            return ""
+        
+        start, end = 0, 0
+        
+        for i in range(len(s)):
+            len1 = self.expand_around_center(s, i, i)      # Odd length palindrome
+            len2 = self.expand_around_center(s, i, i + 1)  # Even length palindrome
+            max_len = max(len1, len2)
+            
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+        
+        return s[start:end + 1]
+
+    def expand_around_center(self, s: str, left: int, right: int) -> int:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
+            
