19fa05b6ab22e1e296de1611b3015fbba003732d
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-08-26 23:19:42 +0000

    Created 'Palindromic Substrings' by A2SV Hub

---
+++ 26-Aug-2024/Palindromic Substrings 208219.py
@@ -0,0 +1,23 @@
+# Problem: Palindromic Substrings - https://leetcode.com/problems/palindromic-substrings/description/
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        count = 0
+        
+        def expandAroundCenter(left: int, right: int):
+            nonlocal count
+            while left >= 0 and right < n and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+        
+        for i in range(n):
+            # Odd length palindromes 
+            expandAroundCenter(i, i)
+            # Even length palindromes
+            expandAroundCenter(i, i + 1)
+
+        return count
+        
+        
