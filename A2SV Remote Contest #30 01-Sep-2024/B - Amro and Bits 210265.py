8a6d64880f26999c97cce3804cc51e1be5241d92
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-09-01 23:32:36 +0000

    Created 'B - Amro and Bits' by A2SV Hub

---
+++ A2SV Remote Contest #30 01-Sep-2024/B - Amro and Bits 210265.py
@@ -0,0 +1,13 @@
+# Problem: B - Amro and Bits - https://codeforces.com/gym/545837/problem/B
+
+def main(x):
+    y = x & -x
+    while True:
+        if (x & y) > 0 and (x ^ y) > 0:
+            return y
+        y += 1
+
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    print(main(x))
