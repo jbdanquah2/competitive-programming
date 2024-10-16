6a20974b59b8150c89d88385aa10e8816aad9138
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-10-16 17:09:26 +0000

    Created 'New Year Transportation' by A2SV Hub

---
+++ 16-Oct-2024/New Year Transportation 220322.py
@@ -0,0 +1,16 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+def can_reach_cell(n, t, portals):
+    current_position = 1  
+
+    while current_position < t:
+        current_position += portals[current_position - 1]  
+
+    return "YES" if current_position == t else "NO"
+
+
+n, t = map(int, input().split())
+portals = list(map(int, input().split()))
+
+result = can_reach_cell(n, t, portals)
+print(result)
