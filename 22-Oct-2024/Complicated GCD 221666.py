d8616f395349d5b104ba95c628848fd05b465726
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2024-10-22 18:40:16 +0000

    Created 'Complicated GCD' by A2SV Hub

---
+++ 22-Oct-2024/Complicated GCD 221666.py
@@ -0,0 +1,13 @@
+# Problem: Complicated GCD - https://codeforces.com/contest/664/problem/A
+
+def gcd_of_range(a, b):
+    if a == b:
+        return a
+    else:
+        return 1
+
+# Input reading
+a, b = input().split()
+
+# Output the result
+print(gcd_of_range(a, b))
